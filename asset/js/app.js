1b63cd3da10ee47ad808abed8e3626c80058c197
romelmahmud <romelmahmud@outlook.com>
2021-05-20 17:11:48 +0600

    fixe dropdown menu

---
+++ asset/js/app.js
@@ -0,0 +1,36 @@
+// selecting hamburger menu
+
+const burgerMenu = document.querySelector(".header__hambuger");
+
+// selecting close icon
+
+const closeMenu = document.querySelector(".header__close");
+
+// toggel function
+
+// burgerMenu.addEventListener("click", () => {
+//   if (burgerMenu.classList.contains("show")) {
+//     burgerMenu.classList.remove("show");
+//     burgerMenu.classList.add("hidden");
+//     closeMenu.classList.remove("hidden");
+//     closeMenu.classList.add("show");
+//   } else if (burgerMenu.classList.contains("hidden")) {
+//     burgerMenu.classList.remove("hidden");
+//     burgerMenu.classList.add("show");
+//     closeMenu.classList.remove("hidden");
+//     closeMenu.classList.add("show");
+//   }
+// });
+// closeMenu.addEventListener("click", () => {
+//   if (closeMenu.classList.contains("show")) {
+//     closeMenu.classList.remove("show");
+//     closeMenu.classList.add("hidden");
+//     burgerMenu.classList.remove("hidden");
+//     burgerMenu.classList.add("show");
+//   } else if (closeMenu.classList.contains("hidden")) {
+//     closeMenu.classList.remove("hidden");
+//     closeMenu.classList.add("show");
+//     burgerMenu.classList.remove("hidden");
+//     burgerMenu.classList.add("show");
+//   }
+// });
